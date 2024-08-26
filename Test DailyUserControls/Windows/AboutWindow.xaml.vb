79c8f8c39aa61f99247fe2187b9e56071c1ce6bd
op2 <operatortwo@bluewin.ch>
2024-08-26 20:54:03 +0200

    V 1.1.0.0  targeting .NET Framework 4.8

---
+++ Test DailyUserControls/Windows/AboutWindow.xaml.vb
@@ -0,0 +1,12 @@
+﻿Public Class AboutWindow
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs)
+        Dim appver As String = My.Application.Info.Version.ToString
+        Dim libname As Reflection.AssemblyName = GetType(DailyUserControls.ImageButton).Assembly.GetName
+        Dim libver As String = libname.Version.ToString
+
+        tbDescription.Text = My.Application.Info.Description
+        tbAppVersion.Text = appver
+        tbLibVersion.Text = libver
+        tbCopyright.Text = My.Application.Info.Copyright
+    End Sub
+End Class
